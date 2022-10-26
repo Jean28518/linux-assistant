59f97464e5a48cb937a5c8b496e9da79b8da4a03
Jean28518 <jf_v@gmx.de>
2022-10-27 00:26:11 +0200

    Fix #21

--- additional/python/get_recent_files.py
+++ additional/python/get_recent_files.py
@@ -1,12 +1,14 @@
 import gi
+import urllib.parse
 
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
 
 recent_manager = Gtk.RecentManager.get_default();
 items = recent_manager.get_items()
+
 for item in items:
     if item and item.exists():
         file_path = item.get_uri().replace("file://", "")
-        file_path = file_path.replace("%20", " ")
+        file_path = urllib.parse.unquote(file_path)
         print(f"{file_path}")
