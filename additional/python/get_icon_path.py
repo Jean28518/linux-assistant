51da4dcb0a2ef7528b312712a4292d9a9e5eb388
Jean28518 <jf_v@gmx.de>
2022-11-21 15:26:42 +0100

    Add path detection to icon path recognizer

--- additional/python/get_icon_path.py
+++ additional/python/get_icon_path.py
@@ -9,6 +9,12 @@ import jfolders
 import jfiles
 
 icon_name = jessentials.get_value_from_arguments("icon")
+
+# Assume path
+if len(icon_name.split("/")) > 3:
+    print(icon_name)
+    exit(0)
+
 icon_theme = Gtk.IconTheme.get_default()
 icon = icon_theme.lookup_icon(icon_name, 128, 0)
 if icon:
