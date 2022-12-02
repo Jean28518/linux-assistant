261950eb730b22f0052cad187eb7c17614cc925f
Jean28518 <jf_v@gmx.de>
2022-12-03 00:41:37 +0100

    Add compression type to deb packager

--- build-deb.sh
+++ build-deb.sh
@@ -5,5 +5,5 @@ cp -r additional build/linux/x64/release/bundle/
 cp -r build/linux/x64/release/bundle/* deb/usr/lib/linux-assistant/
 chmod +x deb/usr/bin/linux-assistant
 chmod 755 deb/DEBIAN
-dpkg-deb --build deb 
+dpkg-deb --build -Zxz deb 
 mv deb.deb linux-assistant.deb
