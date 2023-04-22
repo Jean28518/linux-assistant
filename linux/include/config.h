c316d015820c13b8df4bc90a07760a8197d7f5c4
Nati0ns <117521599+Nati0ns@users.noreply.github.com>
2023-04-22 12:32:59 +0200

    Add version info via command line switch

---
+++ linux/include/config.h
@@ -0,0 +1,11 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define PROJECT_NAME "Linux-Assistant"
+#define PROJECT_DESC "A daily linux helper with powerful integrated search, routines and checks."
+#define PROJECT_VER  "0.2.1"
+#define PROJECT_VER_MAJOR "0"
+#define PROJECT_VER_MINOR "2"
+#define PTOJECT_VER_PATCH "1"
+
+#endif // CONFIG_H
--- linux/main.cc
+++ linux/main.cc
@@ -1,6 +1,25 @@
+#include <iostream>
+#include <string>
+#include "config.h"
 #include "my_application.h"
 
+void print_version_info();
+
 int main(int argc, char** argv) {
+  if (argc > 1){
+    std::string cmdArg(argv[1]);
+    if (cmdArg == "-v" || cmdArg == "--version"){
+      print_version_info();
+      return 0;
+    }
+  }
+
   g_autoptr(MyApplication) app = my_application_new();
   return g_application_run(G_APPLICATION(app), argc, argv);
+}
+
+void print_version_info() {
+  std::cout << PROJECT_NAME << " " << PROJECT_VER << std::endl;
+  std::cout << PROJECT_DESC << std::endl;
+  std::cout << "Homepage: https://www.linux-assistant.org" << std::endl;
 }
