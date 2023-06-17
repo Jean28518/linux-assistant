df73d1224df75637c2f68cafd67471a4a02432d2
Nati0ns <117521599+Nati0ns@users.noreply.github.com>
2023-06-17 15:01:21 +0200

    Port get_diskspace to dart

---
+++ lib/helpers/command_helper.dart
@@ -0,0 +1,28 @@
+import 'dart:io';
+import 'package:tuple/tuple.dart';
+
+abstract class CommandHelper {
+  static Future<Tuple2<bool, String>> run(String cmd,
+      {Map<String, String>? env, bool asRoot = false}) async {
+    return await runWithArguments(cmd, [], env: env, asRoot: asRoot);
+  }
+
+  static Future<Tuple2<bool, String>> runWithArguments(
+      String cmd, List<String> args,
+      {Map<String, String>? env, bool asRoot = false}) async {
+    if (asRoot) {
+      if (args.isEmpty) {
+        args.add(cmd);
+      } else {
+        args.insert(0, cmd);
+      }
+
+      cmd = "pkexec";
+    }
+
+    var procResult = await Process.run(cmd, args, environment: env);
+    return procResult.exitCode == 0
+        ? Tuple2(true, procResult.stdout.toString().trim())
+        : Tuple2(false, procResult.stderr.toString().trim());
+  }
+}
---
+++ lib/linux/linux_filesystem.dart
@@ -0,0 +1,53 @@
+import 'package:linux_assistant/helpers/command_helper.dart';
+
+class Device {
+  final String filesystem;
+  final String mountPoint;
+  final String size;
+  final String sizeFree;
+  final String sizeUsed;
+  final int usedPercent;
+
+  const Device(this.filesystem, this.size, this.sizeUsed, this.sizeFree,
+      this.usedPercent, this.mountPoint);
+}
+
+abstract class LinuxFilesystem {
+  static Future<List<Device>> getDevices() async {
+    var cmdResult = await CommandHelper.runWithArguments("df", ["-h"]);
+    if (!cmdResult.item1) {
+      throw Exception(cmdResult.item2);
+    }
+
+    var ignore = [
+      "df:",
+      "udev",
+      "tmpfs",
+      "/dev/loop",
+      "revokefs-fuse",
+      "cgroup"
+    ];
+
+    var lines = cmdResult.item2
+        .split("\n")
+        .skip(1)
+        .where((x) => !ignore.any((y) => x.startsWith(y)));
+
+    var devices = List<Device>.empty(growable: true);
+    var devicesRead = List<String>.empty(growable: true);
+    for (var line in lines) {
+      var values = line.split(" ");
+      values.removeWhere((x) => x == "");
+      if (devicesRead.contains(values[0]) || values[1].endsWith("M")) {
+        continue;
+      }
+
+      devices.add(Device(values[0], values[1], values[2], values[3],
+          int.parse(values[4].substring(0, values[4].length - 1)), values[5]));
+
+      devicesRead.add(values[0]);
+    }
+
+    return devices;
+  }
+}
