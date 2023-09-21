868734c01b1a5c9b4cd68beb98890596d11dc722
Jean28518 <jf_v@gmx.de>
2023-09-21 20:24:52 +0200

    Update test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,13 +7,16 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:linux_assistant/layouts/greeter/start_screen.dart';
 
 import 'package:linux_assistant/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(MyApp(
+      firstPage: const StartScreen(),
+    ));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
